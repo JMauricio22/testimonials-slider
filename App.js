9a0a448382d08ed33f2040525fb673fe7633096a
Mauricio Lemus <rodeznomauricio@gmail.com>
2020-09-11 22:56:49 -0600

    add slider

--- App.js
+++ App.js
@@ -2,29 +2,27 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Container from "./src/components/Container/Container.js";
 import Card from "./src/components/Card/Card.js";
-import PersonIMG from "./src/images/image-tanya.jpg";
+import PersonIMGTanya from "./src/images/image-tanya.jpg";
+import PersonIMGJohn from "./src/images/image-john.jpg";
 
 const people = [
 	{
-		img: PersonIMG,
-		text:
-			"\"I've been interested in coding for a while but never taken the jump, until now. I couldn't recom		      mend this course enough. I'm now in the job of my dreams and so excited about the future.\"",
+		img: PersonIMGTanya,
+		text:"“I've been interested in coding for a while but never taken the jump, until now. I couldn't recommend th		      is course enough. I'm now in the job of my dreams and so excited about the future. ”",
 		name: "Tanya Sinclair",
 		job: "UX Engineer",
+	},
+	{
+		img: PersonIMGJohn,
+		text:"“ If you want to lay the best foundation possible I’d recommend taking this course.The depth the instrut		      ors go into is incredible. I now feel so confident about starting up as a professional developer. ”",
+		name: " John Tarkpor",
+		job: " Junior Front-end Developer",
 	},
 ];
 function App() {
 	return (
 		<Container>
-			{people.map((person, index) => (
-				<Card
-					key={index}
-					personIMG={person.img}
-					text={person.text}
-					name={person.name}
-					job={person.job}
-				/>
-			))}
+			<Card people={people} />
 		</Container>
 	);
 }
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import IMGNext from "../../images/icon-next.svg";
 import IMGPrev from "../../images/icon-prev.svg";
 import PatternQuotes from "../../images/pattern-quotes.svg";
@@ -6,26 +6,51 @@ import PatternBG from "../../images/pattern-bg.svg";
 import IMGCurve from "../../images/pattern-curve.svg";
 import "./Card.css";
 
-const Card = (props) => {
+const Card = ({ people }) => {
+	const [person, setPerson] = useState(people[0]);
+	const [slideIndex, setSlide] = useState(1);
+
+	const slider = (index) => {
+		if (index > people.length) index = 1;
+		else if (index < 1) index = people.length;
+	 	
+	 	setSlide(index);
+
+		index -= 1;
+
+		setPerson({
+			name: people[index].name,
+			job: people[index].job,
+			text: people[index].text,
+			img: people[index].img,
+		});
+	};
+
 	return (
 		<div className="card">
 			<div className="card__text">
 				<img src={PatternQuotes} alt="quotes" className="card__quotes-img" />
-				<p className="card__paragraph">{props.text}</p>
+				<p className="card__paragraph">{person.text}</p>
 				<div className="card__person">
-					<span className="card__name">{props.name}</span>
-					<span className="card__job">{props.job}</span>
+					<span className="card__name">{person.name}</span>
+					<span className="card__job">{person.job}</span>
 				</div>
 			</div>
 			<div className="card__img">
 				<img src={PatternBG} alt="bg" className="card__bg-img" />
 				<div className="card__container-person-img">
-					<img src={props.personIMG} alt="person" className="card__person-img" />
+					<img src={person.img} alt="person" className="card__person-img" />
 					<div className="card__container-btn">
-						<button className="card__btn card__btn--prev">
+						<button
+							className="card__btn card__btn--prev"
+							onClick={() => slider(slideIndex - 1)}
+						>
 							<img src={IMGPrev} alt="prev" className="card__btn-img" />
 						</button>
-						<button className="card__btn card__btn--next">
+						<button
+							className="card__btn card__btn--next"
+							onClick={() => slider(slideIndex + 1)}
+						>
 							<img src={IMGNext} alt="next" className="card__btn-img" />
 						</button>
 					</div>
